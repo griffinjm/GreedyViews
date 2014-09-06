b94268bdd615d9a38da6b8f631aedcdbe3db6f03
JM Griffin <griffin.john.michael@gmail.com>
2014-09-06 16:25:30 +0100

    Override the default onInterceptTouchEvent method.

--- app/src/main/java/ie/jgriffin/greedyviews/GreedyLinearLayout.java
+++ app/src/main/java/ie/jgriffin/greedyviews/GreedyLinearLayout.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
 /**
@@ -21,5 +22,12 @@ public class GreedyLinearLayout extends LinearLayout {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public GreedyLinearLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        //return super.onInterceptTouchEvent(ev);
+        //prevent the touch event being passed to the children of this view
+        return true;
     }
 }
