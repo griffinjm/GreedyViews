ad82993af19298498902c7db7f14ba05ffbe392d
JM Griffin <griffin.john.michael@gmail.com>
2014-09-13 16:29:30 +0100

    Add ScrollViewFriendlyViewPager class.

---
+++ app/src/main/java/ie/jgriffin/greedyviews/ScrollViewFriendlyViewPager.java
@@ -0,0 +1,21 @@
+package ie.jgriffin.greedyviews;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by JGriffin on 13/09/2014.
+ */
+public class ScrollViewFriendlyViewPager extends ViewPager {
+    
+    public ScrollViewFriendlyViewPager(Context context) {
+        super(context);
+    }
+
+    public ScrollViewFriendlyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
